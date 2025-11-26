f10e25a53043193eab8742db1b1ed89b26403cf2
krishna1tawar <krishnatawar275@gmail.com>
2025-11-26 11:52:04 +0000

    Updated UI + Completed frontend

---
+++ Sensore Project/Controllers/SensorViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sensore_Project.Controllers
+{
+    public class SensorViewController : Controller
+    {
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
